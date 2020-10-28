8a11f6262012e97c7753e3666eff252a9f6054cd
Susmoy <susmoy.cse@gmail.com>
2020-10-28 16:06:22 +0600

    resposive nav 2

---
+++ public/script.js
@@ -0,0 +1,9 @@
+const menu_icon = document.querySelector('#menu-icon');
+const menu = document.querySelector('#menu');
+menu_icon.addEventListener('click', ()=>{
+    if(menu.classList.contains('hidden')){
+        menu.classList.remove('hidden')
+    } else {
+        menu.classList.add('hidden')
+    }
+})
